039d1b2697c3e2571c42513383e0ff1b9c1eba05
Sammatha99 <ngonhatrang99@gmail.com>
2020-08-06 09:34:42 +0700

    delete old code module2

--- bloom1/src/Dictionary/Node/SuggestionDictionay.java
+++
@@ -1,6 +0,0 @@
-package Dictionary.Node;
-
-public interface SuggestionDictionay {
-    public void suggestWord (String Data);
-    public void suggestDiction();
-}
--- bloom1/src/File/FileFilter/FileFilter.java
+++
@@ -1,7 +0,0 @@
-package File.FileFilter;
-
-import File.MyFile;
-
-public interface FileFilter {
-    boolean accept(MyFile pathname);
-}
